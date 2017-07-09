d7428393493807e0aa28a88670dae08b4c823315
1160500810LWY <1119183912@qq.com>
2017-07-09 17:38:00 +0800

    V1.2

--- GuessNumeber.c
+++ GuessNumeber.c
@@ -21,31 +21,46 @@ main(){
         printf("Cannot open file1!\n");
         exit(0);}
         srand( (unsigned)time( NULL ) );
-        int maxnum=100;
         int answer;
         int ans;
-        int cnt=1;
-        answer=rand()%100;
-        printf("\n请输入一个0-99之间的数：\n");
-        fprintf(fp2,"请输入一个0-99之间的数：\n");
-        scanf("%d",&ans);
-        fprintf(fp2,"%d\n",ans);
-        while(ans!=answer){
-           if(ans>answer){
-            printf("你猜的数偏大\n");
-            fprintf(fp2,"你猜的数偏大\n");
-           }
+        int i;
+        int maxnum;
+		printf("\n请选择需要进行的操作，0代表结束游戏，1代表开始游戏：\n"); 
+		fprintf(fp2,"请选择需要进行的操作，0代表结束游戏，1代表开始游戏：\n");
+		scanf("%d",&i);
+		while(i==1){
+			int cnt=1;
+		   	fprintf(fp2,"你选择了开始游戏\n");
+			printf("请输入maxnum的值：\n"); 
+			fprintf(fp2,"请输入maxnum的值：\n");
+			scanf("%d",&maxnum);
+			fprintf(fp2,"%d\n",maxnum);
+        	answer=rand()%maxnum;
+        	printf("请输入一个0-%d之间的数：\n",maxnum);
+        	fprintf(fp2,"请输入一个0-%d之间的数：\n",maxnum);
+        	scanf("%d",&ans);
+        	fprintf(fp2,"%d\n",ans);
+       	 	while(ans!=answer){
+           	if(ans>answer){
+            	printf("你猜的数偏大\n");
+            	fprintf(fp2,"你猜的数偏大\n");
+           	}
             else{
             printf("你猜的数偏小\n");
             fprintf(fp2,"你猜的数偏小\n");
             }
-            printf("请输入一个新的0-99之间的数：\n");
-            fprintf(fp2,"请输入一个新的0-99之间的数：\n");
+            printf("请输入一个新的0-%d之间的数：\n",maxnum);
+            fprintf(fp2,"请输入一个新的0-%d之间的数：\n",maxnum);
             scanf("%d",&ans);
             fprintf(fp2,"%d\n",ans);
             cnt=cnt+1;
         }
         printf("恭喜你猜对了，共猜了%d次，正确答案为%d\n",cnt,answer);
-        fprintf(fp2,"恭喜你猜对了，共猜了%d次，正确答案为%d\n",cnt,answer); 
-        fclose(fp2);
+        fprintf(fp2,"恭喜你猜对了，共猜了%d次，正确答案为%d\n",cnt,answer);
+    printf("请选择需要进行的操作，0代表结束游戏，1代表继续游戏：\n");
+    fprintf(fp2,"请选择需要进行的操作，0代表结束游戏，1代表继续游戏：\n");
+    scanf("%d",&i); 
+	}
+	fprintf(fp2,"你选择了结束游戏\n");
+	fclose(fp2);
 }
